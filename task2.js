3516b72340ea6623373af1bdb636b97dbff0932d
Ifedayo Ijabadeniyi <ifedayoadeniyi@gmail.com>
2021-08-18 11:56:03 +0100

    Complete seccond task.  🍫

---
+++ task2.js
@@ -0,0 +1,11 @@
+const name = "Ijabadeniyi Ifedayo";
+
+/**
+ * Print user's name.
+ * @param {string} name Name to print.
+ */
+const printName = (name) => {
+  console.log(name);
+};
+
+printName(name);
